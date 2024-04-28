723581a22397c1b0e394771c5eb94de335fe3b27
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2024-04-28 12:33:50 -0600

    GroupId -> GroupRef

--- lib/syntax_definitions/syntax_definition.dart
+++ lib/syntax_definitions/syntax_definition.dart
@@ -17,11 +17,11 @@ abstract class SyntaxDefinition {
     return (newExpr, tracker);
   }
 
-  SyntaxResult capture(SyntaxResult inner, [GroupId? id]) {
+  SyntaxResult capture(SyntaxResult inner, [GroupRef? ref]) {
     var (innerExpr, tracker) = inner;
     tracker = tracker.increment();
-    if (id != null) {
-      tracker = tracker.startTracking(id);
+    if (ref != null) {
+      tracker = tracker.startTracking(ref);
     }
     return ("($innerExpr)", tracker);
   }
@@ -96,50 +96,50 @@ abstract class SyntaxDefinition {
 
 typedef SyntaxResult = (String, GroupTracker);
 
-class GroupId {}
+class GroupRef {}
 
 // TODO: document operation meanings
 class GroupTracker {
-  final Map<GroupId, int> _positions;
+  final Map<GroupRef, int> _positions;
   final int _groupCount;
 
   const GroupTracker._create(this._positions, this._groupCount);
   const GroupTracker(): this._create(const {}, 0);
 
-  GroupTracker startTracking(GroupId newId, [int position = 1]) {
-    assert (!_positions.containsKey(newId));
+  GroupTracker startTracking(GroupRef newRef, [int position = 1]) {
+    assert (!_positions.containsKey(newRef));
     return GroupTracker._create(
       {
         ..._positions,
-        newId: position,
+        newRef: position,
       },
       _groupCount,
     );
   }
 
-  int getPosition(GroupId id) {
-    if (!_positions.containsKey(id)) throw ArgumentError("Position not tracked for id!");
-    return _positions[id]!;
+  int getPosition(GroupRef ref) {
+    if (!_positions.containsKey(ref)) throw ArgumentError("Position not tracked for ref!");
+    return _positions[ref]!;
   }
 
   int get groupCount => _groupCount;
 
   GroupTracker increment([int by = 1]) => GroupTracker._create(
     {
-      for (var MapEntry(key: groupId, value: position) in _positions.entries)
-        groupId: position + by,
+      for (var MapEntry(key: groupRef, value: position) in _positions.entries)
+        groupRef: position + by,
     },
     _groupCount + by,
   );
 
   static GroupTracker combine(List<GroupTracker> trackers) {
-    var combinedPositions = <GroupId, int>{};
+    var combinedPositions = <GroupRef, int>{};
     var totalGroupCount = 0;
 
     for (var tracker in trackers) {
-      for (var MapEntry(key: groupId, value: position) in tracker._positions.entries) {
-        assert (!combinedPositions.containsKey(groupId));
-        combinedPositions[groupId] = position + totalGroupCount;
+      for (var MapEntry(key: groupRef, value: position) in tracker._positions.entries) {
+        assert (!combinedPositions.containsKey(groupRef));
+        combinedPositions[groupRef] = position + totalGroupCount;
       }
       totalGroupCount += tracker._groupCount;
     }
--- test/syntax_definition_test.dart
+++ test/syntax_definition_test.dart
@@ -1,40 +1,39 @@
 import 'package:test/test.dart';
 
 import '../lib/syntax_definitions/syntax_definition.dart';
-import '../lib/syntax_printer.dart';
 
 
 void main() {
   group("capture index tracking, like", () {
     test("a single capture group", () {
-      var id = GroupId();
-      var (expr, tracker) = TestDefinition().singleCaptureTest1(id);
+      var ref = GroupRef();
+      var (expr, tracker) = TestDefinition().singleCaptureTest1(ref);
       expect(expr, equals("(asdf)"));
-      expect(tracker.getPosition(id), equals(1));
+      expect(tracker.getPosition(ref), equals(1));
     });
 
     test("nested capture groups", () {
-      var id1 = GroupId();
-      var id2 = GroupId();
-      var id3 = GroupId();
-      var (expr, tracker) = TestDefinition().multiCaptureTest1(id1, id2, id3);
+      var ref1 = GroupRef();
+      var ref2 = GroupRef();
+      var ref3 = GroupRef();
+      var (expr, tracker) = TestDefinition().multiCaptureTest1(ref1, ref2, ref3);
       expect(expr, equals("(((bfksn)))"));
-      expect(tracker.getPosition(id1), equals(1));
-      expect(tracker.getPosition(id2), equals(2));
-      expect(tracker.getPosition(id3), equals(3));
+      expect(tracker.getPosition(ref1), equals(1));
+      expect(tracker.getPosition(ref2), equals(2));
+      expect(tracker.getPosition(ref3), equals(3));
     });
 
     test("nested and concatenated capture groups", () {
-      var id1 = GroupId();
-      var id2 = GroupId();
-      var id3 = GroupId();
-      var id4 = GroupId();
-      var (expr, tracker) = TestDefinition().multiCaptureTest2(id1, id2, id3, id4);
+      var ref1 = GroupRef();
+      var ref2 = GroupRef();
+      var ref3 = GroupRef();
+      var ref4 = GroupRef();
+      var (expr, tracker) = TestDefinition().multiCaptureTest2(ref1, ref2, ref3, ref4);
       expect(expr, equals("(abc1)abc2(((abc3))abc4)"));
-      expect(tracker.getPosition(id1), equals(1));
-      expect(tracker.getPosition(id2), equals(2));
-      expect(tracker.getPosition(id3), equals(3));
-      expect(tracker.getPosition(id4), equals(4));
+      expect(tracker.getPosition(ref1), equals(1));
+      expect(tracker.getPosition(ref2), equals(2));
+      expect(tracker.getPosition(ref3), equals(3));
+      expect(tracker.getPosition(ref4), equals(4));
     });
   });
 }
@@ -43,22 +42,22 @@ class TestDefinition extends SyntaxDefinition {
   @override
   get body => throw UnimplementedError();
 
-  SyntaxResult singleCaptureTest1(GroupId id) {
-    return capture(pattern("asdf"), id);
+  SyntaxResult singleCaptureTest1(GroupRef ref) {
+    return capture(pattern("asdf"), ref);
   }
 
-  SyntaxResult multiCaptureTest1(GroupId id1, GroupId id2, GroupId id3) {
-    return capture(capture(capture(pattern("bfksn"), id3), id2), id1);
+  SyntaxResult multiCaptureTest1(GroupRef ref1, GroupRef ref2, GroupRef ref3) {
+    return capture(capture(capture(pattern("bfksn"), ref3), ref2), ref1);
   }
 
-  SyntaxResult multiCaptureTest2(GroupId id1, GroupId id2, GroupId id3, GroupId id4) {
+  SyntaxResult multiCaptureTest2(GroupRef ref1, GroupRef ref2, GroupRef ref3, GroupRef ref4) {
     return concat([
-      capture(pattern("abc1"), id1),
+      capture(pattern("abc1"), ref1),
       pattern("abc2"),
       capture(concat([
-        capture(capture(pattern("abc3"), id4), id3),
+        capture(capture(pattern("abc3"), ref4), ref3),
         pattern("abc4")
-      ]), id2)
+      ]), ref2)
     ]);
   }
 }
