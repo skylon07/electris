77a984208f32d2891a2f4c6221de253c42ae0792
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2024-05-02 01:30:59 -0600

    Add method to create definition items in a "smart" way

--- lib/syntax_definitions/syntax_definition.dart
+++ lib/syntax_definitions/syntax_definition.dart
@@ -24,11 +24,11 @@ abstract base class SyntaxDefinition<CollectionT extends RegExpCollection> {
     repository: [for (var item in _items) item.asRepositoryItem()],
   );
 
-  DefinitionItem createItem(
+  DefinitionItem createItemDirect(
     String identifier,
     {
       required Pattern Function(String debugName, List<Pattern> innerPatterns) createBody,
-      List<DefinitionItem> Function()? createInnerItems,
+      List<DefinitionItem>? Function()? createInnerItems,
     }
   ) => DefinitionItem(
     identifier,
@@ -36,13 +36,115 @@ abstract base class SyntaxDefinition<CollectionT extends RegExpCollection> {
     createBody: createBody,
     createInnerItems: createInnerItems,
   );
+
+  DefinitionItem createItem(
+    String identifier,
+    {
+      StyleName? styleName,
+      String? match,
+      String? begin,
+      String? end,
+      List<CapturePattern>? captures,
+      List<CapturePattern>? beginCaptures,
+      List<CapturePattern>? endCaptures,
+      List<DefinitionItem>? Function()? createInnerItems,
+    }
+  ) {
+    var argMap = {
+      'debugName': null, // set later
+      'styleName': styleName,
+      'match': match,
+      'begin': begin,
+      'end': end,
+      'captures': captures,
+      'beginCaptures': beginCaptures,
+      'endCaptures': endCaptures,
+      'innerPatterns': null, // set later
+    };
+    return createItemDirect(
+      identifier,
+      createBody: (debugName, innerPatterns) {
+        argMap['debugName'] = debugName;
+        argMap['innerPatterns'] = innerPatterns;
+        return _createItem_smartBody(argMap);
+      },
+      createInnerItems: createInnerItems,
+    );
+  }
+
+  Pattern _createItem_smartBody(Map<String, dynamic> argMap) =>
+    switch (argMap) {
+      {
+        'debugName': String debugName,
+        'match': String match,
+
+        'styleName': StyleName? styleName,
+        'captures': List<CapturePattern>? captures,
+        
+        'begin': null,
+        'end': null,
+        'beginCaptures': null,
+        'endCaptures': null,
+        'innerPatterns': null || [],
+      } => 
+        MatchPattern(
+          debugName: debugName,
+          styleName: styleName,
+          match: match,
+          captures: captures ?? [],
+        ),
+
+      {
+        'debugName': String debugName,
+        'begin': String begin,
+        'end': String end,
+
+        'styleName': StyleName? styleName,
+        'beginCaptures': List<CapturePattern>? beginCaptures,
+        'endCaptures': List<CapturePattern>? endCaptures,
+        'innerPatterns': List<Pattern>? innerPatterns,
+
+        'match': null,
+        'captures': null,
+      } =>
+        EnclosurePattern(
+          debugName: debugName,
+          styleName: styleName,
+          innerPatterns: innerPatterns ?? [],
+          begin: begin,
+          end: end,
+          beginCaptures: beginCaptures ?? [],
+          endCaptures: endCaptures ?? [],
+        ),
+
+      {
+        'debugName': String debugName,
+        'innerPatterns': List<Pattern> innerPatterns,
+
+        'styleName': StyleName? styleName,
+
+        'match': null,
+        'begin': null,
+        'end': null,
+        'captures': null,
+        'beginCaptures': null,
+        'endCaptures': null,
+      } when innerPatterns.isNotEmpty =>
+        GroupingPattern(
+          debugName: debugName,
+          styleName: styleName,
+          innerPatterns: innerPatterns
+        ),
+      
+      _ => throw ArgumentError("Invalid argument pattern"),
+    };
 }
 
 final class DefinitionItem {
   final SyntaxDefinition parent;
   final String identifier;
   final Pattern Function(String debugName, List<Pattern> innerPatterns) createBody;
-  final List<DefinitionItem> Function()? createInnerItems;
+  final List<DefinitionItem>? Function()? createInnerItems;
 
   DefinitionItem(
     this.identifier,
@@ -60,7 +162,9 @@ final class DefinitionItem {
     identifier: identifier,
     body: createBody(
       "${parent.langName}.$identifier",
-      [for (var item in innerItems) item.asIncludePattern()]
+      innerItems
+        .map((item) => item.asIncludePattern())
+        .toList(),
     )
   );
 
