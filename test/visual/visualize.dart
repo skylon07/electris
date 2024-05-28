0f726feee7a258cb8ad5c731c6d6284e7068c713
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2024-05-28 09:25:10 -0600

    Add variable visualizations (normal, Type, and CONST)

--- test/visual/visualize.dart
+++ test/visual/visualize.dart
@@ -5,3 +5,18 @@ part of 'something';
   part of 'something';
   part    of 'something';
 partof 'something';
+
+void main() {
+  var myVariable;
+  var __$_myVariable_yay;
+  
+  var __$_MyType_yay;
+  var __$_MY_TYPE_yay;
+  var T;
+
+  var __$_MY_CONST_YAY;
+  var CN;
+  
+  var vars = Type? "str-literal" : int;
+  vars = null;
+}
