8872362474bf2b03f7e455843fd472b6ce5fbf1a
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2024-06-15 07:55:20 -0600

    Reorganize to use a finalization function on all public-facing recipe builder methods

--- lib/better_textmate/regexp_builder_base.dart
+++ lib/better_textmate/regexp_builder_base.dart
@@ -5,10 +5,12 @@ import './regexp_normalization.dart';
 abstract base class RegExpBuilder<CollectionT> {
   CollectionT createCollection();
 
+  RegExpRecipe _finalize(RegExpRecipe recipe) => normalize(recipe);
+
   // base/fundamental recipe creation functions
 
   RegExpRecipe exactly(String string) => 
-    _escapedPattern(string, RegExp(r"[.?*+\-()\[\]{}\^$|]"));
+    _finalize(_escapedPattern(string, RegExp(r"[.?*+\-()\[\]{}\^$|]")));
 
   RegExpRecipe _escapedPattern(String exprStr, RegExp? escapeExpr) {
     String expr;
@@ -17,62 +19,62 @@ abstract base class RegExpBuilder<CollectionT> {
     } else {
       expr = exprStr;
     }
-    return normalize(BaseRegExpRecipe(expr));
+    return BaseRegExpRecipe(expr);
   }
 
-  RegExpRecipe chars(String charSet) => _chars(charSet, invert: false);
+  RegExpRecipe chars(String charSet) => _finalize(_chars(charSet, invert: false));
 
-  RegExpRecipe notChars(String charSet) => _chars(charSet, invert: true);
+  RegExpRecipe notChars(String charSet) => _finalize(_chars(charSet, invert: true));
 
   RegExpRecipe _chars(String charSet, {bool invert = false}) {
     var baseRecipe = _escapedPattern(charSet, RegExp(r"[\[\]\^\/]"));
     var augment = (expr) => "[${invert? "^":""}$expr]";
-    return normalize(InvertibleRegExpRecipe(baseRecipe, augment, inverted: invert, tag: RegExpTag.chars));
+    return InvertibleRegExpRecipe(baseRecipe, augment, inverted: invert, tag: RegExpTag.chars);
   }
 
   // basic composition operations
 
   RegExpRecipe capture(RegExpRecipe inner, [GroupRef? ref]) {
     var augment = (expr) => "($expr)";
-    return normalize(TrackedRegExpRecipe(inner, augment, ref: ref, tag: RegExpTag.capture));
+    return _finalize(TrackedRegExpRecipe(inner, augment, ref: ref, tag: RegExpTag.capture));
   }
 
-  RegExpRecipe concat(List<RegExpRecipe> recipes) => capture(_join(recipes, joinBy: "", tag: RegExpTag.concat));
+  RegExpRecipe concat(List<RegExpRecipe> recipes) => _finalize(capture(_join(recipes, joinBy: "", tag: RegExpTag.concat)));
 
   RegExpRecipe _augment(RegExpRecipe recipe, String Function(String expr) mapExpr, {RegExpTag tag = RegExpTag.none}) =>
-    normalize(AugmentedRegExpRecipe(recipe, mapExpr, tag: tag));
+    AugmentedRegExpRecipe(recipe, mapExpr, tag: tag);
 
   RegExpRecipe _join(List<RegExpRecipe> recipes, {required String joinBy, RegExpTag tag = RegExpTag.none}) {
     if (recipes.isEmpty) throw ArgumentError("Joining list should not be empty.", "recipes"); 
-    return normalize(JoinedRegExpRecipe(recipes, joinBy, tag: tag));
+    return JoinedRegExpRecipe(recipes, joinBy, tag: tag);
   }
 
 
   // repitition and optionality
   
   RegExpRecipe optional(RegExpRecipe inner) =>
-    _augment(inner, (expr) => "$expr?");
+    _finalize(_augment(inner, (expr) => "$expr?"));
 
   RegExpRecipe zeroOrMore(RegExpRecipe inner) =>
-    _augment(inner, (expr) => "$expr*");
+    _finalize(_augment(inner, (expr) => "$expr*"));
 
   RegExpRecipe oneOrMore(RegExpRecipe inner) =>
-    _augment(inner, (expr) => "$expr+");
+    _finalize(_augment(inner, (expr) => "$expr+"));
 
   RegExpRecipe repeatEqual(RegExpRecipe inner, int times) =>
-    _augment(inner, (expr) => "$expr{$times}");
+    _finalize(_augment(inner, (expr) => "$expr{$times}"));
     
   RegExpRecipe repeatAtLeast(RegExpRecipe inner, int times) =>
-    _augment(inner, (expr) => "$expr{$times,}");
+    _finalize(_augment(inner, (expr) => "$expr{$times,}"));
 
   RegExpRecipe repeatAtMost(RegExpRecipe inner, int times) =>
-    _augment(inner, (expr) => "$expr{,$times}");
+    _finalize(_augment(inner, (expr) => "$expr{,$times}"));
 
   RegExpRecipe repeatBetween(RegExpRecipe inner, int lowTimes, int highTimes) =>
-    _augment(inner, (expr) => "$expr{$lowTimes,$highTimes}");
+    _finalize(_augment(inner, (expr) => "$expr{$lowTimes,$highTimes}"));
 
   RegExpRecipe either(List<RegExpRecipe> branches) =>
-    capture(_join(branches, joinBy: r"|", tag: RegExpTag.either));
+    _finalize(capture(_join(branches, joinBy: r"|", tag: RegExpTag.either)));
 
   late final nothing = exactly("");
 
@@ -80,28 +82,28 @@ abstract base class RegExpBuilder<CollectionT> {
   // "look around" operations
 
   RegExpRecipe aheadIs(RegExpRecipe inner) => 
-    _augment(inner, (expr) => "(?=$expr)", tag: RegExpTag.aheadIs);
+    _finalize(_augment(inner, (expr) => "(?=$expr)", tag: RegExpTag.aheadIs));
 
   RegExpRecipe aheadIsNot(RegExpRecipe inner) => 
-    _augment(inner, (expr) => "(?!$expr)", tag: RegExpTag.aheadIsNot);
+    _finalize(_augment(inner, (expr) => "(?!$expr)", tag: RegExpTag.aheadIsNot));
 
   RegExpRecipe behindIs(RegExpRecipe inner) => 
-    _augment(inner, (expr) => "(?<=$expr)", tag: RegExpTag.behindIs);
+    _finalize(_augment(inner, (expr) => "(?<=$expr)", tag: RegExpTag.behindIs));
 
   RegExpRecipe behindIsNot(RegExpRecipe inner) => 
-    _augment(inner, (expr) => "(?<!$expr)", tag: RegExpTag.behindIsNot);
+    _finalize(_augment(inner, (expr) => "(?<!$expr)", tag: RegExpTag.behindIsNot));
 
   
   // anchors
 
   RegExpRecipe startsWith(RegExpRecipe inner) =>
-    _augment(inner, (expr) => "^$expr");
+    _finalize(_augment(inner, (expr) => "^$expr"));
 
   RegExpRecipe endsWith(RegExpRecipe inner) =>
-    _augment(inner, (expr) => "$expr\$");
+    _finalize(_augment(inner, (expr) => "$expr\$"));
 
   RegExpRecipe startsAndEndsWith(RegExpRecipe inner) =>
-    _augment(inner, (expr) => "^$expr\$");
+    _finalize(_augment(inner, (expr) => "^$expr\$"));
 
 
   // spacing
@@ -109,38 +111,39 @@ abstract base class RegExpBuilder<CollectionT> {
   late final _anySpace = _escapedPattern(r"\s*", null);
   late final _reqSpace = _escapedPattern(r"\s+", null);
 
-  RegExpRecipe space({required bool req}) => req? _reqSpace : _anySpace;
+  RegExpRecipe space({required bool req}) => 
+    _finalize(req? _reqSpace : _anySpace);
 
   RegExpRecipe spaceBefore(RegExpRecipe inner) =>
-    concat([_anySpace, inner]);
+    _finalize(concat([_anySpace, inner]));
   
   RegExpRecipe spaceReqBefore(RegExpRecipe inner) =>
-    concat([_reqSpace, inner]);
+    _finalize(concat([_reqSpace, inner]));
 
   RegExpRecipe spaceAfter(RegExpRecipe inner) =>
-    concat([inner, _anySpace]);
+    _finalize(concat([inner, _anySpace]));
   
   RegExpRecipe spaceReqAfter(RegExpRecipe inner) =>
-    concat([inner, _reqSpace]);
+    _finalize(concat([inner, _reqSpace]));
     
   RegExpRecipe spaceAround(RegExpRecipe inner) =>
-    concat([_anySpace, inner, _anySpace]);
+    _finalize(concat([_anySpace, inner, _anySpace]));
   
   RegExpRecipe spaceReqAround(RegExpRecipe inner) =>
-    concat([_reqSpace, inner, _reqSpace]);
+    _finalize(concat([_reqSpace, inner, _reqSpace]));
 
   RegExpRecipe spaceAroundReqBefore(RegExpRecipe inner) =>
-    concat([_reqSpace, inner, _anySpace]);
+    _finalize(concat([_reqSpace, inner, _anySpace]));
   
   RegExpRecipe spaceAroundReqAfter(RegExpRecipe inner) =>
-    concat([_anySpace, inner, _reqSpace]);
+    _finalize(concat([_anySpace, inner, _reqSpace]));
 
   RegExpRecipe phrase(String string) {
     var inner = _augment(
       exactly(string),
       (expr) => expr.replaceAll(r" ", r"\s+"),
     );
-    return concat([
+    return _finalize(concat([
       behindIs(either([
         startsWith(nothing),
         space(req: true),
@@ -149,7 +152,7 @@ abstract base class RegExpBuilder<CollectionT> {
         concat([inner, aheadIs(_nonWordChar)]),
         endsWith(inner),
       ]),
-    ]);
+    ]));
   }
 
   late final _nonWordChar = notChars(r"a-zA-Z0-9_$"); // dart chars -- should work for most languages
