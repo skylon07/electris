04d802d49da6a1570b322d8298a4ceddbb62bb71
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2024-06-15 12:00:54 -0600

    Move style enum down

--- lib/syntax_definitions/dart_definition.dart
+++ lib/syntax_definitions/dart_definition.dart
@@ -103,60 +103,6 @@ final class DartDefinition extends SyntaxDefinition<DartRegExpCollector, DartReg
 }
 
 
-// TODO: needs to be moved to a different file that is for more generalized things
-enum ElectrisStyleName implements StyleName {
-  sourceCode_variable("source-code.variable"),
-  sourceCode_operator("source-code.operator"),
-  sourceCode_primitiveLiteral("source-code.primitive-literal"),
-  sourceCode_escape("source-code.escape"),
-  sourceCode_comment("source-code.comment"),
-  sourceCode_documentation("source-code.documentation"),
-  sourceCode_types_type("source-code.types.type"),
-  sourceCode_types_typeRecursive("source-code.types.type-recursive"),
-  sourceCode_functionCall("source-code.function-call"),
-  sourceCode_punctuation("source-code.types.punctuation"),
-  
-  text("text"),
-  sourceText("source-text"),
-
-  markdown_styleOperator("markdown.style-operator"),
-  markdown_bold("markdown.bold"),
-  markdown_emphasized("markdown.emphasized"),
-  markdown_strikethrough("markdown.strikethrough"),
-  markdown_heading("markdown.heading"),
-  markdown_horizontalRule("markdown.horizontal-rule"),
-  markdown_code("markdown.code"),
-  markdown_codeLang("markdown.code-lang"),
-  markdown_imageLink("markdown.image-link"),
-  markdown_linkPath("markdown.link-path"),
-  markdown_linkHoverTitle("markdown.link-hover-title"),
-  markdown_escape("markdown.escape"),
-  markdown_extended_styleOperator("markdown.extended.style-operator"),
-  markdown_extended_highlighted("markdown.extended.highlighted"),
-  markdown_extended_subscript("markdown.extended.subscript"),
-
-  xml_tagBrace("xml.tag-brace"),
-  xml_tagLabel("xml.tagLabel"),
-  xml_property("xml.property"),
-  xml_propertyPunctuation("xml.property-punctuation"),
-  xml_propertyInvalid("xml.property-invalid"),
-  xml_propertyValue("xml.property-value"),
-  xml_propertyAssignment("xml.property-assignment"),
-  xml_comment("xml.comment"),
-
-  gitignore_comment("gitignore.comment"),
-  gitignore_file("gitignore.file"),
-  gitignore_punctuation("gitignore.punctuation"),
-  gitignore_operator("gitignore.operator");
-
-  
-  final String _scope;
-  String get scope => "electris.$_scope";
-
-  const ElectrisStyleName(this._scope);
-}
-
-
 final class DartRegExpCollector extends RegExpBuilder<DartRegExpCollector> {
   late final RegExpRecipe variablePlain;
   late final RegExpRecipe variableType;
@@ -301,4 +247,58 @@ final class DartRegExpCollector extends RegExpBuilder<DartRegExpCollector> {
 
     return this;
   }
+}
+
+
+// TODO: needs to be moved to a different file that is for more generalized things
+enum ElectrisStyleName implements StyleName {
+  sourceCode_variable("source-code.variable"),
+  sourceCode_operator("source-code.operator"),
+  sourceCode_primitiveLiteral("source-code.primitive-literal"),
+  sourceCode_escape("source-code.escape"),
+  sourceCode_comment("source-code.comment"),
+  sourceCode_documentation("source-code.documentation"),
+  sourceCode_types_type("source-code.types.type"),
+  sourceCode_types_typeRecursive("source-code.types.type-recursive"),
+  sourceCode_functionCall("source-code.function-call"),
+  sourceCode_punctuation("source-code.types.punctuation"),
+  
+  text("text"),
+  sourceText("source-text"),
+
+  markdown_styleOperator("markdown.style-operator"),
+  markdown_bold("markdown.bold"),
+  markdown_emphasized("markdown.emphasized"),
+  markdown_strikethrough("markdown.strikethrough"),
+  markdown_heading("markdown.heading"),
+  markdown_horizontalRule("markdown.horizontal-rule"),
+  markdown_code("markdown.code"),
+  markdown_codeLang("markdown.code-lang"),
+  markdown_imageLink("markdown.image-link"),
+  markdown_linkPath("markdown.link-path"),
+  markdown_linkHoverTitle("markdown.link-hover-title"),
+  markdown_escape("markdown.escape"),
+  markdown_extended_styleOperator("markdown.extended.style-operator"),
+  markdown_extended_highlighted("markdown.extended.highlighted"),
+  markdown_extended_subscript("markdown.extended.subscript"),
+
+  xml_tagBrace("xml.tag-brace"),
+  xml_tagLabel("xml.tagLabel"),
+  xml_property("xml.property"),
+  xml_propertyPunctuation("xml.property-punctuation"),
+  xml_propertyInvalid("xml.property-invalid"),
+  xml_propertyValue("xml.property-value"),
+  xml_propertyAssignment("xml.property-assignment"),
+  xml_comment("xml.comment"),
+
+  gitignore_comment("gitignore.comment"),
+  gitignore_file("gitignore.file"),
+  gitignore_punctuation("gitignore.punctuation"),
+  gitignore_operator("gitignore.operator");
+
+  
+  final String _scope;
+  String get scope => "electris.$_scope";
+
+  const ElectrisStyleName(this._scope);
 }
